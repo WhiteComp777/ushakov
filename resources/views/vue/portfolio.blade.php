eb76844ee537c1f84c70104e1fe555d91457ac55
white <whitecomp777@yandex.ru>
2016-06-26 01:17:56 +0300

    portfolio css

--- resources/views/vue/portfolio.blade.php
+++ resources/views/vue/portfolio.blade.php
@@ -4,13 +4,14 @@
 	<div class="portfolio" >
 
 <article v-for="portfolio in portfolios" >
+<div class="square sqr"></div>
+<div class="square sql"></div>
 		<div class="images">
 			<img v-bind:src="portfolio.image" alt="">
 		</div>
 		<header>
 		<a href="#" v-link="{name: 'portfolioItem', params: { PortfolioId: portfolio.id }}">
 		<h3>@{{portfolio.name}}</h3>
-		<h4>тяжелый сайт</h4>
 		</a>
 	</header>
 </article>
