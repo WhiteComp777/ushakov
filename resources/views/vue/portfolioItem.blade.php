f95cad319d503c50971c1e18d63cf5e234a3b7fa
white <whitecomp777@yandex.ru>
2016-06-26 01:29:51 +0300

    portfolio css

--- resources/views/vue/portfolioItem.blade.php
+++ resources/views/vue/portfolioItem.blade.php
@@ -2,7 +2,7 @@
 <section class="portfolio_inner">
 
 <div class="pItem">
-	<h1>@{{ portfolio.name }} <a href="@{{portfolio.url}}" class="btn-open">открыть</a></h1>
+	<h1>@{{ portfolio.name }} <a href="@{{portfolio.url}}" class="btn-open" target="_blank">открыть</a></h1>
 	<div class="images">
 		<img v-bind:src="portfolio.image" alt="">
 	</div>
