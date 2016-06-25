4a685d988b898c1e23720bf47d7a4eefb5cb192c
white <whitecomp777@yandex.ru>
2016-06-26 02:00:34 +0300

    vueport

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	 <meta name="viewport" content="width=device-width">
+	 <meta name="viewport" content="width=device-width" id="mediaWidth">
 
 	<link rel="stylesheet" href="css/nprogress.css">
 	<link rel="stylesheet" href="css/style.css">
@@ -59,8 +59,17 @@
 
 
 {{-- @include('vue.call') --}}
+
 <template id="contactsTPL"> @include("vue.contacts") </template>
 
+<script>
+if(window.innerWidth<500){
+viewport = document.querySelector("meta[name=viewport]");
+viewport.setAttribute('content', 'width=500');
+}
+// document.getElementById("mediaWidth");
+	
+</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.25/vue.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-router/0.7.13/vue-router.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.8.0/vue-resource.js"></script>
